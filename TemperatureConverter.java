e49ddcdfcde5fa37353236c1709626567fe74993
OWLIVERR <jadhavpiyush987@gmail.com>
2023-12-04 15:16:36 +0530

    added f -> c also

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -8,11 +8,19 @@ public class Main{
         System.out.println("Enter the temperature in Celsius : ");
         float c = sc.nextFloat();
         
-        // Converting Celsius to Fahrenheit
+        
         float f = c * (9.0f/5.0f) + 32;
         
         System.out.println("The temperature is "+f+" degree Fahrenheit.");
-        
+      
+
+         Scanner input = new Scanner(System.in); 
+         System.out.print("Enter a temperature in Fahrenheit: "); 
+      	 double fahrenheit = input.nextDouble(); 
+         input.close(); 
+         double celsius = (fahrenheit - 32) * 5 / 9; 
+         System.out.println(fahrenheit + " F is equivalent to " + celsius + " C");  
+
     }
     
 } 
